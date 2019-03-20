c05d73f646c7e79d884ef3720bd13a7d437440a9
Derick Anderson <anderson.derick.w@gmail.com>
2019-03-20 10:09:18 -0400

    Check for game over conditions

--- pentago/pentago/game.py
+++ pentago/pentago/game.py
@@ -15,6 +15,8 @@ BOARD_DIMS = [BOARD_H, BOARD_W]
 WHITE_TURN = 1
 BLACK_TURN = -1
 
+N_IN_A_ROW = 5
+
 Board = np.ndarray
 # dims = [BOARD_H, BOARD_W], dtype = np.uint8
 # values of 0 represent a blank space
@@ -33,36 +35,53 @@ Rotation = np.ndarray
 # all values are zero except that corresponding to the rotated quadrant
 # the nonzero value is 1 for clockwise rotation and -1 for counterclockwise
 
-Move = typing.Tuple[Placement, Rotation]
-
 
 class Game():
+    """A nice wrapper class for game related functionality.
+
+    All real logic is in module methods.
+
+    """
     def __init__(self):
-        self.board: Board = _generate_clean_board()
+        self.board: Board = generate_clean_board()
         self.turn = WHITE_TURN
 
-    def make_move(self, move: Move):
-        self.board = _apply_move(self.board, self.turn, move)
+    def make_move(self, placement, rotation):
+        self.board = apply_move(self.board, self.turn, placement, rotation)
         self.turn *= -1
 
+    def check_game_over(self) -> typing.Optional[int]:
+        """Returns WHITE_TURN or BLACK_TURN if the respective player has won, 0 if the
+        game is a draw, and None if the game is not over.
 
-def _generate_clean_board():
-    return np.zeros(BOARD_DIMS, dtype=np.int8)
+        """
+        return check_game_over(self.board)
 
 
-def _can_place_mask(board):
+@numba.jit(numba.int8[:, :]())
+def generate_clean_board():
+    return np.zeros((BOARD_H, BOARD_W), dtype=np.int8)
+
+
+@numba.jit(numba.boolean[:, :](numba.int8[:, :],))
+def can_place_mask(board):
     return board == 0
 
 
-@numba.jit()
-def _apply_move(board, turn, move: Move):
-    placement, rotation = move
-    board = board + placement.astype(np.int8) * turn
-    _apply_rotation(board, rotation)
-    return board
-
-
 @numba.jit(nopython=True)
+def _make_swaps(board, q_i, q_j, swaps, direction):
+    for i in range(2):
+        from_i = swaps[i * 2][0] + q_i * 3
+        from_j = swaps[i * 2][1] + q_j * 3
+        to_i = swaps[(i * 2 + direction) % 4][0] + q_i * 3
+        to_j = swaps[(i * 2 + direction) % 4][1] + q_j * 3
+
+        temp = board[to_i, to_j]
+        board[to_i, to_j] = board[from_i, from_j]
+        board[from_i, from_j] = temp
+
+
+@numba.jit((numba.int8[:, :], numba.int8[:, :]), nopython=True)
 def _apply_rotation(board, rotation):
     q_i = 0
     q_j = 0
@@ -80,14 +99,70 @@ def _apply_rotation(board, rotation):
     _make_swaps(board, q_i, q_j, corners, direction)
 
 
-@numba.jit(nopython=True)
-def _make_swaps(board, q_i, q_j, swaps, direction):
-    for i in range(2):
-        from_i = swaps[i * 2][0] + q_i * 3
-        from_j = swaps[i * 2][1] + q_j * 3
-        to_i = swaps[(i * 2 + direction) % 4][0] + q_i * 3
-        to_j = swaps[(i * 2 + direction) % 4][1] + q_j * 3
+@numba.jit(numba.int8[:, :](numba.int8[:, :],
+                            numba.int8,
+                            numba.boolean[:, :],
+                            numba.int8[:, :]),
+           nopython=True)
+def apply_move(board, turn, placement, rotation):
+    board = board + placement.astype(np.int8) * turn
+    _apply_rotation(board, rotation)
+    return board
 
-        temp = board[to_i, to_j]
-        board[to_i, to_j] = board[from_i, from_j]
-        board[from_i, from_j] = temp
+
+@numba.jit()
+def _check_line(board, i, j, i_stride, j_stride):
+    sign = 0
+    while i < BOARD_H and j < BOARD_W:
+        if board[i, j] != sign:
+            run = 0
+            sign = board[i, j]
+        run += 1
+        if run == 5 and sign != 0:
+            return sign
+        i += i_stride
+        j += j_stride
+    return 0
+
+
+@numba.jit(numba.optional(numba.int32)(numba.int8[:, :],), nopython=True)
+def check_victory(board):
+    # check rows
+    for i in range(BOARD_H):
+        r = _check_line(board, i, 0, 0, 1)
+        if r != 0:
+            return r
+
+    # check columns
+    for j in range(BOARD_W):
+        r = _check_line(board, 0, j, 1, 0)
+        if r != 0:
+            return r
+
+    # check diagonals
+    diagonals = [_check_line(board, 0, 0, 1, 1),
+                 _check_line(board, 1, 0, 1, 1),
+                 _check_line(board, 0, 1, 1, 1),
+                 _check_line(board, 5, 0, -1, 1),
+                 _check_line(board, 4, 0, -1, 1),
+                 _check_line(board, 5, 1, -1, 1)]
+    for i in range(6):
+        if diagonals[i] != 0:
+            return diagonals[i]
+
+
+@numba.jit(numba.boolean(numba.int8[:, :],), nopython=True)
+def check_draw(board):
+    return np.all(board)
+
+
+@numba.jit(numba.optional(numba.int32)(numba.int8[:, :],), nopython=True)
+def check_game_over(board):
+    victory = check_victory(board)
+    draw = check_draw(board)
+    if victory is not None:
+        return victory
+    elif draw:
+        return 0
+    else:
+        return None
--- pentago/tests/test_board.py
+++ pentago/tests/test_board.py
@@ -1,45 +1,84 @@
 import numpy as np
 
 import pentago
+import pentago.game
 
 
 def test_move():
     game = pentago.Game()
     assert game.turn == 1
-    placement = np.zeros_like(game.board)
+    placement = np.zeros_like(game.board, dtype=np.bool)
     placement[0, 0] = 1
     rotation = np.asarray([[1, 0],
                            [0, 0]],
                           dtype=np.int8)
-    game.make_move((placement, rotation))
+    game.make_move(placement, rotation)
     expected_board = np.zeros_like(game.board)
     expected_board[2, 0] = 1
     assert np.all(game.board == expected_board)
 
-    placement = np.zeros_like(game.board)
+    placement = np.zeros_like(game.board, dtype=np.bool)
     placement[5, 5] = 1
     rotation = np.asarray([[0, 0],
                            [0, -1]],
                           dtype=np.int8)
-    game.make_move((placement, rotation))
+    game.make_move(placement, rotation)
     expected_board[5, 3] = -1
     assert np.all(game.board == expected_board)
 
-    placement = np.zeros_like(game.board)
+    placement = np.zeros_like(game.board, dtype=np.bool)
     placement[1, 3] = 1
     rotation = np.asarray([[0, 1],
                            [0, 0]],
                           dtype=np.int8)
-    game.make_move((placement, rotation))
+    game.make_move(placement, rotation)
     expected_board[2, 4] = 1
     assert np.all(game.board == expected_board)
 
-    placement = np.zeros_like(game.board)
+    placement = np.zeros_like(game.board, dtype=np.bool)
     placement[3, 2] = 1
     rotation = np.asarray([[0, 0],
                            [-1, 0]],
                           dtype=np.int8)
-    game.make_move((placement, rotation))
+    game.make_move(placement, rotation)
     expected_board[3, 0] = -1
     assert np.all(game.board == expected_board)
     
+
+def test_check_game_over():
+    assert pentago.game.check_game_over(pentago.game.generate_clean_board()) is None
+
+    board = pentago.game.generate_clean_board()
+    board[0, 0] = 1
+    board[0, 1] = 1
+    board[0, 2] = 1
+    board[0, 3] = 1
+    board[0, 4] = 1
+    assert pentago.game.check_game_over(board) == 1
+
+    board = -1 * board
+    assert pentago.game.check_game_over(board) == -1
+
+    board = pentago.game.generate_clean_board()
+    board[0, 0] = 1
+    board[0, 1] = 1
+    board[0, 2] = 1
+    board[0, 3] = 1
+    assert pentago.game.check_game_over(board) is None
+
+    board = pentago.game.generate_clean_board()
+    board[0, 0] = 1
+    board[1, 1] = 1
+    board[2, 2] = 1
+    board[3, 3] = 1
+    board[4, 4] = 1
+    assert pentago.game.check_game_over(board) == 1
+
+    board = np.asarray([[ 1,  1,  1, -1,  1,  1],  # noqa
+                        [ 1,  1,  1, -1,  1,  1],  # noqa
+                        [ 1,  1,  1, -1,  1, -1],  # noqa
+                        [-1, -1, -1,  1, -1,  1],  # noqa
+                        [ 1,  1, -1,  1, -1,  1],  # noqa
+                        [ 1,  1,  1, -1,  1,  1]],   # noqa
+                       dtype=np.int8)
+    assert pentago.game.check_game_over(board) == 0
