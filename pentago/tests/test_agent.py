78f114b10eab40a4a425490afe21c871085d5728
Derick Anderson <anderson.derick.w@gmail.com>
2019-03-29 19:45:49 -0400

    Rename search_ai.py to minimax.py

--- pentago/tests/test_agent.py
+++ pentago/tests/test_agent.py
@@ -3,17 +3,17 @@ import json
 
 import pentago
 import pentago.agent
-import pentago.search_ai
+import pentago.minimax
 
 
 def test_get_agent_for_key():
     assert pentago.get_agent_for_key(None, 'human') is None
     assert type(pentago.get_agent_for_key(None, 'random')) == pentago.agent.RandomAgent
-    assert type(pentago.get_agent_for_key(None, 'minimax')) == pentago.search_ai.MinimaxSearchAgent
+    assert type(pentago.get_agent_for_key(None, 'minimax')) == pentago.minimax.MinimaxSearchAgent
 
     with pytest.raises(NameError):
         pentago.get_agent_for_key(None, 'foo')
 
-    agent = pentago.search_ai.MinimaxSearchAgent(depth=3)
+    agent = pentago.minimax.MinimaxSearchAgent(depth=3)
     config = json.loads(json.dumps(agent.to_params()))
     assert pentago.get_agent_for_key(None, 'minimax', config).depth == 3
